b221f8d32c84317ca25c8fdc8a0f0c6412e8ba9c
sagar <sagarslife222@gmail.com>
2026-05-25 13:42:30 +0530

    fixed cors and api url

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://erp-iasm.vercel.app', credentials: true }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
